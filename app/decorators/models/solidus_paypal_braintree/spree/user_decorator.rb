e529907f639d9cdbe7d4fb69a1963b7421a471b6
Anton Kopylov <anton@kopylov.net>
2020-04-08 13:35:51 +0300

    Fix decorators auto load

--- app/decorators/models/solidus_paypal_braintree/spree/user_decorator.rb
+++ app/decorators/models/solidus_paypal_braintree/spree/user_decorator.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
-module Models
-  module SolidusPaypalBraintree
-    module Spree
-      module UserDecorator
-        def self.prepended(base)
-          base.has_one :braintree_customer, class_name: 'SolidusPaypalBraintree::Customer', inverse_of: :user
-        end
-
-        ::Spree.user_class.prepend self
+module SolidusPaypalBraintree
+  module Spree
+    module UserDecorator
+      def self.prepended(base)
+        base.has_one :braintree_customer, class_name: 'SolidusPaypalBraintree::Customer', inverse_of: :user
       end
+
+      ::Spree.user_class.prepend self
     end
   end
 end
